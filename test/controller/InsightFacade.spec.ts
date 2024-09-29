73a605ad301bdc51b2542f6cc0db22587a41ddc2
UBC Student <dfrendol@students.cs.ubc.ca>
2024-09-29 13:44:01 -0700

    add test(dilon)

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -1,4 +1,11 @@
-import { IInsightFacade, InsightDatasetKind, InsightError, InsightResult } from "../../src/controller/IInsightFacade";
+import {
+	IInsightFacade,
+	InsightDatasetKind,
+	InsightError,
+	InsightResult,
+	NotFoundError,
+	ResultTooLargeError,
+} from "../../src/controller/IInsightFacade";
 import InsightFacade from "../../src/controller/InsightFacade";
 import { clearDisk, getContentFromArchives, loadTestQuery } from "../TestUtil";
 
@@ -19,10 +26,12 @@ describe("InsightFacade", function () {
 
 	// Declare datasets used in tests. You should add more datasets like this!
 	let sections: string;
+	let sections1: string;
 
 	before(async function () {
 		// This block runs once and loads the datasets.
 		sections = await getContentFromArchives("pair.zip");
+		sections1 = await getContentFromArchives("onevalid.zip");
 
 		// Just in case there is anything hanging around from a previous run of the test suite
 		await clearDisk();
@@ -44,10 +53,237 @@ describe("InsightFacade", function () {
 		it("should reject with  an empty dataset id", async function () {
 			try {
 				await facade.addDataset("", sections, InsightDatasetKind.Sections);
+				expect.fail("Should not accept an empty dataset id");
 			} catch (err) {
 				expect(err).to.be.instanceOf(InsightError);
 			}
-			expect.fail("Should have thrown above.");
+		});
+
+		it("should reject id starts with _", async function () {
+			try {
+				await facade.addDataset("_DEF", sections, InsightDatasetKind.Sections);
+				expect.fail("should reject id starts with _");
+			} catch (err) {
+				expect(err).to.be.instanceOf(InsightError);
+			}
+		});
+
+		it("should reject id with _ in the middle", async function () {
+			try {
+				await facade.addDataset("D_EF", sections, InsightDatasetKind.Sections);
+				expect.fail("should reject id with _ in the middle");
+			} catch (err) {
+				expect(err).to.be.instanceOf(InsightError);
+			}
+		});
+
+		it("should reject id with _ in the end", async function () {
+			try {
+				await facade.addDataset("DEF_", sections, InsightDatasetKind.Sections);
+				expect.fail("should reject id with _ in the end");
+			} catch (err) {
+				expect(err).to.be.instanceOf(InsightError);
+			}
+		});
+
+		it("should reject id with only whitespace", async function () {
+			try {
+				await facade.addDataset("  ", sections, InsightDatasetKind.Sections);
+				expect.fail("should reject id with only whitespace");
+			} catch (err) {
+				expect(err).to.be.instanceOf(InsightError);
+			}
+		});
+
+		it("should added successfully", async function () {
+			try {
+				const set1 = await facade.addDataset("CPSC310", sections, InsightDatasetKind.Sections);
+				expect(set1).to.have.members(["CPSC310"]);
+			} catch (err) {
+				expect.fail("should not fail");
+			}
+		});
+
+		it("should reject dataset with no valid course", async function () {
+			try {
+				const set1 = await facade.addDataset("CPSC313", sections1, InsightDatasetKind.Sections);
+				expect.fail("should reject dataset with no valid course");
+			} catch (err) {
+				expect(err).to.be.instanceOf(InsightError);
+			}
+		});
+	});
+
+	describe("RemoveDataset", function () {
+		beforeEach(function () {
+			// This section resets the insightFacade instance
+			// This runs before each test
+			facade = new InsightFacade();
+		});
+
+		afterEach(async function () {
+			// This section resets the data directory (removing any cached data)
+			// This runs after each test, which should make each test independent of the previous one
+			await clearDisk();
+		});
+
+		it("reject with an empty dataset id", async function () {
+			try {
+				await facade.removeDataset("");
+				expect.fail("Should not accept an empty dataset id");
+			} catch (err) {
+				expect(err).to.be.instanceOf(InsightError);
+			}
+		});
+
+		it("reject id starts with _", async function () {
+			try {
+				await facade.removeDataset("_DEF");
+				expect.fail("should reject id starts with _");
+			} catch (err) {
+				expect(err).to.be.instanceOf(InsightError);
+			}
+		});
+
+		it("reject id with _ in the middle", async function () {
+			try {
+				await facade.removeDataset("D_EF");
+				expect.fail("should reject id with _ in the middle");
+			} catch (err) {
+				expect(err).to.be.instanceOf(InsightError);
+			}
+		});
+
+		it("reject id with _ in the end", async function () {
+			try {
+				await facade.removeDataset("DEF_");
+				expect.fail("should reject id with _ in the end");
+			} catch (err) {
+				expect(err).to.be.instanceOf(InsightError);
+			}
+		});
+
+		it("reject id with only whitespace", async function () {
+			try {
+				await facade.removeDataset("  ");
+				expect.fail("should reject id with only whitespace");
+			} catch (err) {
+				expect(err).to.be.instanceOf(InsightError);
+			}
+		});
+
+		it("should have removed successfully", async function () {
+			try {
+				await facade.addDataset("CPSC310", sections, InsightDatasetKind.Sections);
+				const set = await facade.removeDataset("CPSC310");
+				expect(set).to.equal("CPSC310");
+			} catch (err) {
+				expect.fail("should removed successfully");
+			}
+		});
+
+		it("cannot find id", async function () {
+			try {
+				await facade.addDataset("CPSC310", sections, InsightDatasetKind.Sections);
+				await facade.removeDataset("CPSC");
+				expect.fail("cannot find id");
+			} catch (err) {
+				expect(err).to.be.instanceOf(NotFoundError);
+			}
+		});
+
+		it("cannot remove empty dataset", async function () {
+			try {
+				await facade.removeDataset("CPSC");
+				expect.fail("cannot remove from empty dataset");
+			} catch (err) {
+				expect(err).to.be.instanceOf(NotFoundError);
+			}
+		});
+
+		it("remove same dataset twice", async function () {
+			try {
+				await facade.addDataset("CPSC310", sections, InsightDatasetKind.Sections);
+				await facade.removeDataset("CPSC310");
+				await facade.removeDataset("CPSC310");
+				expect.fail("should not remove twice");
+			} catch (err) {
+				expect(err).to.be.instanceOf(NotFoundError);
+			}
+		});
+
+		it("should have removed multiple successfully", async function () {
+			try {
+				await facade.addDataset("CPSC310", sections, InsightDatasetKind.Sections);
+				await facade.addDataset("CPSC313", sections, InsightDatasetKind.Sections);
+				const set = await facade.removeDataset("CPSC310");
+				expect(set).to.equal("CPSC310");
+				const set1 = await facade.removeDataset("CPSC313");
+				expect(set1).to.equal("CPSC313");
+			} catch (err) {
+				expect.fail("should removed successfully");
+			}
+		});
+	});
+
+	describe("ListDataset", function () {
+		beforeEach(function () {
+			// This section resets the insightFacade instance
+			// This runs before each test
+			facade = new InsightFacade();
+		});
+
+		afterEach(async function () {
+			// This section resets the data directory (removing any cached data)
+			// This runs after each test, which should make each test independent of the previous one
+			await clearDisk();
+		});
+
+		it("should list nothing", async function () {
+			try {
+				const set = await facade.listDatasets();
+				expect(set).to.deep.equal([]);
+			} catch (err) {
+				expect.fail("Should not fail");
+			}
+		});
+
+		it("should list one dataset", async function () {
+			try {
+				await facade.addDataset("CPSC310", sections, InsightDatasetKind.Sections);
+				const set = await facade.listDatasets();
+				expect(set).to.deep.equal([
+					{
+						id: "CPSC310",
+						kind: InsightDatasetKind.Sections,
+						numRows: 64612,
+					},
+				]);
+			} catch (err) {
+				expect.fail("Should not fail");
+			}
+		});
+
+		it("should list multiple datasets", async function () {
+			try {
+				await facade.addDataset("ubc", sections, InsightDatasetKind.Sections);
+				await facade.addDataset("CPSC310", sections, InsightDatasetKind.Sections);
+				const set = await facade.listDatasets();
+				expect(set).to.deep.equal([
+					{
+						id: "ubc",
+						kind: InsightDatasetKind.Sections,
+						numRows: 64612,
+					},
+					{
+						id: "CPSC310",
+						kind: InsightDatasetKind.Sections,
+						numRows: 64612,
+					},
+				]);
+			} catch (err) {
+				expect.fail("Should not fail");
+			}
 		});
 	});
 
@@ -67,23 +303,27 @@ describe("InsightFacade", function () {
 			}
 			// Destructuring assignment to reduce property accesses
 			const { input, expected, errorExpected } = await loadTestQuery(this.test.title);
-			let result: InsightResult[] = []; // dummy value before being reassigned
+			let result: InsightResult[];
 			try {
 				result = await facade.performQuery(input);
+				if (errorExpected) {
+					expect.fail(`performQuery resolved when it should have rejected with ${expected}`);
+				}
+				expect(result).to.deep.equal(expected);
+				expect(input).to.be.instanceOf(Object);
 			} catch (err) {
 				if (!errorExpected) {
 					expect.fail(`performQuery threw unexpected error: ${err}`);
 				}
-				// TODO: replace this failing assertion with your assertions. You will need to reason about the code in this function
-				// to determine what to put here :)
-				return expect.fail("Write your assertion(s) here.");
+				if (expected === "InsightError") {
+					expect(err).to.be.instanceOf(InsightError);
+				} else if (expected === "ResultTooLargeError") {
+					expect(err).to.be.instanceOf(ResultTooLargeError);
+				} else {
+					expect.fail("should not reach here");
+				}
 			}
-			if (errorExpected) {
-				expect.fail(`performQuery resolved when it should have rejected with ${expected}`);
-			}
-			// TODO: replace this failing assertion with your assertions. You will need to reason about the code in this function
-			// to determine what to put here :)
-			return expect.fail("Write your assertion(s) here.");
+			// return expect.fail("Write your assertion(s) here."); // TODO: replace with your assertions
 		}
 
 		before(async function () {
@@ -110,5 +350,38 @@ describe("InsightFacade", function () {
 		// The relative path to the query file must be given in square brackets.
 		it("[valid/simple.json] SELECT dept, avg WHERE avg > 97", checkQuery);
 		it("[invalid/invalid.json] Query missing WHERE", checkQuery);
+		it(
+			"[invalid/asteriskmiddle.json] SELECT dept, instructor WHERE instructor contains asterisk in the middle",
+			checkQuery
+		);
+		it("[invalid/nooptions.json] SELECT with missing OPTIONS field", checkQuery);
+		it(
+			"[valid/asteriskstart.json] SELECT dept, instructor WHERE instructor contains asterisk in the start",
+			checkQuery
+		);
+		it("[valid/asteriskend.json] SELECT dept, instructor WHERE instructor contains asterisk in the end", checkQuery);
+		it(
+			"[valid/asteriskstartend.json] SELECT dept, instructor WHERE instructor contains asterisk at start and end",
+			checkQuery
+		);
+		it("[valid/noasterisk.json] SELECT dept, instructor WHERE instructor contains no asterisk", checkQuery);
+		it("[invalid/resulttoolarge.json] SELECT dept, result too large", checkQuery);
+		it("[valid/lessthan.json] SELECT dept, avg WHERE avg < 25", checkQuery);
+		it("[valid/equal.json] SELECT dept, avg WHERE avg = 98", checkQuery);
+		it("[valid/negation.json] try negation", checkQuery);
+		it("[valid/andcompare.json] and comparator", checkQuery);
+		it("[valid/orcompare.json] or comparator", checkQuery);
+		it("[valid/combinecompare.json] combine comparator", checkQuery);
+		it("[valid/emptyresult.json] empty result", checkQuery);
+		it("[invalid/nocolumn.json] no column", checkQuery);
+		it("[invalid/emptycolumn.json] empty column", checkQuery);
+		it("[invalid/invalidwhere.json] invalid comparator", checkQuery);
+		it("[invalid/multwhere.json] multi dataset in where", checkQuery);
+		it("[invalid/multoption.json] multi dataset in option", checkQuery);
+		it("[invalid/multiple.json] multi dataset in option and where", checkQuery);
+		it("[invalid/noinput.json] no input", checkQuery);
+		it("[invalid/orderfail.json] order not in column", checkQuery);
+		it("[invalid/invalidmkey.json] wrong m key", checkQuery);
+		it("[invalid/invalidskey.json] wrong s key", checkQuery);
 	});
 });
